1cf1cf9c74aebc749a1e636408d789628e967d98
sanjif-shanmugavelu <sshanmugavelu@groq.com>
2024-12-24 18:13:39 +0000

    added bash script to add directory structure. TODO: edit script to remove hardcoded directories if needed

---
+++ create_directory.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir -p probs/finetune/quantized/eval
+mkdir -p probs/finetune/quantized/train
+mkdir -p probs/finetune/fullbits/train
+mkdir -p probs/finetune/fullbits/eval
+mkdir -p probs/base/quantized/eval
+mkdir -p probs/base/quantized/train
+mkdir -p probs/base/fullbits/train
+mkdir -p probs/base/fullbits/eval
+
